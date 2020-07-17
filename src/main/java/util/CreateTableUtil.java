c9b68898f55a60fa5505f3ff71c3a936d9848861
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-07-17 14:24:19 +0300

    implement UserDao for h2 database

--- src/main/java/util/CreateTableUtil.java
+++ src/main/java/util/CreateTableUtil.java
@@ -8,7 +8,7 @@ public class CreateTableUtil {
 
     private static final String createTableUsers =
             "create table if not exists users ("
-                    + "id integer primary key,"
+                    + "id integer primary key auto_increment,"
                     + " username varchar(20),"
                     + " password varchar(20),"
                     + " role varchar(20),"
@@ -16,17 +16,27 @@ public class CreateTableUtil {
 
     private static final String createTableProduct =
             "create table if not exists products ("
-                    + "id integer primary key,"
+                    + "id integer primary key auto_increment,"
                     + " name varchar(20),"
+                    + " description varchar(20),"
                     + " price float,"
                     + " amount float);";
 
     private static final String createTableOrder =
             "create table if not exists orders ("
-                    + "id integer primary key,"
+                    + "id integer primary key auto_increment,"
                     + " user_id integer,"
                     + " order_status varchar(20),"
-                    + " foreign key (user_id) references users(id));";
+                    + " foreign key (user_id) references users(id) on delete cascade);";
+
+    private static final String createTableProductList =
+            "create table if not exists product_list ("
+                    + "order_id integer,"
+                    + " product_id integer,"
+                    + " amount integer,"
+                    + " foreign key (order_id) references orders(id) on delete cascade,"
+                    + " foreign key (product_id) references products(id) on delete cascade,"
+                    + " primary key (order_id, product_id));";
 
     public void run() {
         createTables();
@@ -36,12 +46,14 @@ public class CreateTableUtil {
         System.out.println(createTableUsers);
         System.out.println(createTableProduct);
         System.out.println(createTableOrder);
+        System.out.println(createTableProductList);
         try (Connection connection = H2JDBCUtil.getConnection();
              Statement statement = connection.createStatement()
         ) {
             statement.execute(createTableUsers);
             statement.execute(createTableProduct);
             statement.execute(createTableOrder);
+            statement.execute(createTableProductList);
         } catch (SQLException e) {
             e.printStackTrace();
         }
