66640ff238800298eb6941be9a9956e4764fa9c7
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-07-29 00:24:55 +0300

    remove comennted method from Menu

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -3,8 +3,6 @@ package view;
 public interface Menu {
     void show();
 
-    //void exit();
-
     default void showItems(String[] items) {
         for (String item : items) {
             System.out.println("-------------");
