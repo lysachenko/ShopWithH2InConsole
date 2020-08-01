0de0b286e9e2bdd3f2633fca3dc4ce72944627cb
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-08-01 19:45:11 +0300

    add Shopping cart

---
+++ src/main/java/model/ShoppingCart.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ShoppingCart {
+
+    private Map<Product, Integer> positionMap = new HashMap<>();
+
+    public Map<Product, Integer> getPositionMap() {
+        return positionMap;
+    }
+
+    public void setPositionMap(Map<Product, Integer> positionMap) {
+        this.positionMap = positionMap;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" + positionMap.entrySet().stream()
+                .map(productIntegerEntry ->
+                        "\n\t Product: " + productIntegerEntry.getKey().toString()
+                                + ", amount: " + productIntegerEntry.getValue().toString())
+                .collect(Collectors.joining())
+                + '}';
+    }
+}
