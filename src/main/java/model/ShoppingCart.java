d26da54751c9c8397403c0a516d50ac68408c619
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-08-04 16:18:59 +0300

    add field ID to Shopping catr

--- src/main/java/model/ShoppingCart.java
+++ src/main/java/model/ShoppingCart.java
@@ -1,12 +1,20 @@
 package model;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ShoppingCart {
 
+    private long id;
     private Map<Product, Integer> positionMap;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public Map<Product, Integer> getPositionMap() {
         return positionMap;
